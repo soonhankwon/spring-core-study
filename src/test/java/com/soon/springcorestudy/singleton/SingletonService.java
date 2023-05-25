4309cf5fb166bd3f624509b57dcdfd35f739ce39
soonable@naver.com <soonable@naver.com>
2023-05-25 18:20:32 +0900

    feat : 싱글톤 패턴

---
+++ src/test/java/com/soon/springcorestudy/singleton/SingletonService.java
@@ -0,0 +1,23 @@
+package com.soon.springcorestudy.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonService {
+
+    // jvm 이 static 영역에 객체를 딱 1개 생성
+    private static final SingletonService instance = new SingletonService();
+
+    // public 으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 생성자를 private 로 선언해서 외부에서 new 키워드를 사용한 객체 생성을 못하게 막는다.
+    private SingletonService() {
+    }
+
+    public void logic() {
+        System.out.println("print singleton instance logic");
+    }
+}
