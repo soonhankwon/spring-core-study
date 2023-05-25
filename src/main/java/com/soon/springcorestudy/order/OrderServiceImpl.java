8fe5511ca54c6c53e93657ef7d70861b52340ac9
soonable@naver.com <soonable@naver.com>
2023-05-25 10:51:16 +0900

    feat : 새로운 할인 정책 적용과 문제점 (NPE)

--- src/main/java/com/soon/springcorestudy/order/OrderServiceImpl.java
+++ src/main/java/com/soon/springcorestudy/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.soon.springcorestudy.order;
 
 import com.soon.springcorestudy.discount.DiscountPolicy;
-import com.soon.springcorestudy.discount.FixDiscountPolicy;
 import com.soon.springcorestudy.member.Member;
 import com.soon.springcorestudy.member.MemberRepository;
 import com.soon.springcorestudy.member.MemoryMemberRepository;
@@ -9,7 +8,7 @@ import com.soon.springcorestudy.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
