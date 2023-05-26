fa364942ee13faa00b4b76554c5f95d19b465814
soonable@naver.com <soonable@naver.com>
2023-05-27 00:05:28 +0900

    feat : 스코프와 프록시

---
+++ src/main/java/com/soon/springcorestudy/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package com.soon.springcorestudy.web;
+
+import com.soon.springcorestudy.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
